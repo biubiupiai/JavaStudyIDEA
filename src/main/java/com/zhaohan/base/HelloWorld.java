d9f26f60a0491ebd2e178a30b9d333c198d0a9b2
biubiupiai <1515292574@qq.com>
2020-08-10 16:26:24 +0800

    hello world

---
+++ src/main/java/com/zhaohan/base/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.zhaohan.base;
+
+/**
+ * @Author zhaohan
+ * @Version 1.0
+ * @CreateTime 2020-08-10-16:25
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
